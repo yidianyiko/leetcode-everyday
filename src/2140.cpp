3a74087acda558cf75ec3b44b788e711e9e0ce70
yidianyiko <yidianyiko@foxmail.com>
2025-04-01 21:55:03 +0800

    one day code

---
+++ src/2140.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    long long mostPoints(vector<vector<int>>& questions) {
+        //解法1：暴力，复杂度n^2
+        //解法2：动态规划
+        int n = questions.size();
+        vector<long long> dp(n + 1);
+        for (int i = n - 1; i >= 0; --i){
+            dp[i] = max(dp[i + 1], questions[i][0] + dp[min(n, i + questions[i][1] + 1)]);
+        }
+        return dp[0];
+    }
+};
